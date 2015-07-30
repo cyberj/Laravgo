7c002961b227b5c2670959c21858e82eec04f38e
Johan Charpentier <cyberj@arcagenis.org>
2015-07-30 17:21:57 +0200

    Add birthday via migration

--- app/Contact.php
+++ app/Contact.php
@@ -9,7 +9,7 @@ class Contact extends Model
   // MASS ASSIGNMENT -------------------------------------------------------
   // define which attributes are mass assignable (for security)
   // we only want these 3 attributes able to be filled
-  protected $fillable = array('first_name', 'last_name');
+  protected $fillable = array('first_name', 'last_name', 'birthday');
 
   // One contact have many emails
   public function emails()
